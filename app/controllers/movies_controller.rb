3624ba146d85e9a4ea00187ac2c517e98c684e66
EC2 Default User <ec2-user@ip-172-31-93-77.ec2.internal>
2020-02-07 17:58:44 +0000

    part3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,9 +10,69 @@ class MoviesController < ApplicationController
     # will render app/views/movies/show.<extension> by default
   end
   
+  def allRatings
+    @all_ratings = Movie.order(:rating).select(:rating)
+    @all_ratings = @all_ratings.map(&:rating).uniq
+  end
+  
   def index
-    if params[:order]
-      @movies = Movie.order(params[:order])
+    allRatings
+    if(!params[:ratings].nil?)
+      session[:ratings] = params[:ratings]
+    end
+    if(params[:order].nil?)
+      session[:order] = params[:order] 
+    end
+    if(!session[:ratings].nil? || !session[:order].nil?)
+      #session rating not nill -- session order not nill
+      if(!session[:ratings].nil? && !session[:order.nil?])
+        #param rating nill -- param order nill
+        if (params[:ratings].nil? && params[:order].nil?)
+          @selected = session[:ratings].keys
+          @movies = Movie.where(:rating => @selected)
+          @movies = @movies.order(session[:order])
+        #param rating not nill -- param order nill  
+        elsif (params[:order].nil?)
+          @selected = params[:ratings].keys
+          @movies = Movie.where(:rating => @selected)
+          @movies = @movies.order(session[:order])
+        #param rating nill -- param order not nill
+        else
+          @selected = session[:ratings].keys
+          @movies = Movie.where(:rating => @selected)
+          @movies = @movies.order(params[:order])
+        end
+      #session rating not nill -- session order nill
+      elsif(!session[:ratings].nil?)
+        #param rating nill -- param order nill
+        if (params[:ratings].nil? && params[:order].nil?)
+          @selected = session[:ratings].keys
+          @movies = Movie.where(:rating => @selected)
+        #param rating not nill -- param order nill  
+        elsif (params[:order].nil?)
+          @selected = params[:ratings].keys
+          @movies = Movie.where(:rating => @selected)
+        #param rating nill -- param order not nill
+        else
+          @selected = session[:ratings].keys
+          @movies = Movie.where(:rating => @selected)
+          @movies = @movies.order(params[:order])
+        end
+      #session rating nill -- session order not nill
+      else
+        #param rating nill -- param order nill
+        if (params[:ratings].nil? && params[:order].nil?)
+          @movies = @movies.order(session[:order])
+        #param rating not nill -- param order nill
+        elsif (params[:order].nil?)
+          @selected = params[:ratings].keys
+          @movies = Movie.where(:rating => @selected)
+          @movies = @movies.order(session[:order])
+        #param rating nill -- param order not nill
+        else
+          @movies = @movies.order(params[:order])
+        end
+      end
     else
       @movies = Movie.all
     end
@@ -44,13 +104,5 @@ class MoviesController < ApplicationController
     @movie.destroy
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
-  end
-
-  def check
-    if params[:ratings]
-      params[:ratings].keys
-    else
-      @all_ratings
-    end
   end
 end
--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -3,4 +3,10 @@ module MoviesHelper
   def oddness(count)
     count.odd? ?  "odd" :  "even"
   end
+  def chosen_rating?(rating)
+    chosen_ratings = session[:ratings]
+    return true if chosen_ratings.nil?
+    chosen_ratings.include? rating
+  end
+  
 end
